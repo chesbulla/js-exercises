b85213618f8646bc440f3e28627674a94043341d
chesbulla <chesterchua722@gmail.com>
2024-08-01 05:33:01 +0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce(((total, item) => total + item), 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce(((total, item) => total * item), 1);
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let result = 1;
+  for (let i = 1; i <= a; i++ ) result *= i;
+  return result;
+
 	
 };
 
