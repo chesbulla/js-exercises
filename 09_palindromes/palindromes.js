e367fe838be10a304b1ba80ecd0dedc0faf7c80c
chesbulla <chesterchua722@gmail.com>
2024-08-01 05:47:52 +0800

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    //remove spaces and punctuations
+    s = s.toLowerCase();
+    s = s.split("");
+    s = s.filter((item) => (item.charCodeAt(0) >= 97 && item.charCodeAt(0) <=122) || (item.charCodeAt(0) >= 48 && item.charCodeAt(0) <=57));
+    let reversedS = s.toReversed();
+    return reversedS.toString() === s.toString();
+    
+
+    
+
+
+
 
 };
 
