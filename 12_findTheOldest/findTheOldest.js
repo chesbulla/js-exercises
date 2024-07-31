cce442a3769e21dec08c181f81cf4cad60c9b31a
chesbulla <chesterchua722@gmail.com>
2024-08-01 06:10:33 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    //sort the age
+    //get the last name
+
+    let sorted =  arr.sort((a,b) => {
+        let aAge;
+        let bAge;
+       
+
+        if (!a.yearOfDeath) {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+
+        } else aAge = a.yearOfDeath - a.yearOfBirth;
+
+        if (!b.yearOfDeath) {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+
+        } else bAge = b.yearOfDeath - b.yearOfBirth;
+
+        if (aAge > bAge) return 1;
+        else return -1;
+    });
+    return sorted[sorted.length-1];
+
+
+
+
 
 };
 
